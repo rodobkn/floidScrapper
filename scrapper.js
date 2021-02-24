eef61057a0a4fd0a259e329f739b13c677deac78
rodobkn <rrmendoza@uc.cl>
2021-02-24 12:18:46 -0300

    some change

--- scrapper.js
+++ scrapper.js
@@ -10,7 +10,6 @@ const executeScrap = async (rut, password) => {
   const browser = await puppeteer.launch({
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox',
     ],
   });;
   const page = await browser.newPage();
