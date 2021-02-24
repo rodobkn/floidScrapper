c8de04d58f1e5bda1b34555e8e82fdd6adb34c56
rodobkn <rrmendoza@uc.cl>
2021-02-24 11:52:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const scrapper = require('./scrapper');
+
+const { PORT = 5000 } = process.env;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json()); //To transform the incoming request to json.
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.post('/scrapping', async (req, res) => {
+  try {
+    const { rut, password } = req.body;
+    const balance = await scrapper.executeScrap(rut, password);
+    console.log('Balance: ', balance);
+    res.send({ balance });
+  } catch (error) {
+    console.log(error);
+    res.send({ error: 'Something went wrong. Make sure to write in a good way your credentials' })
+  }
+});
+
+app.listen(PORT, (err) => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
---
+++ scrapper.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer-extra');
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const executeScrap = async (rut, password) => {
+
+  const chileBankUrl = 'https://portales.bancochile.cl/personas';
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(chileBankUrl);
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('#btn_bch_menu_banco-en-linea')
+  ]);
+  await page.type('#iduserName', rut)
+  await page.type('input[name=userpassword]', password)
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('#idIngresar')
+  ]);
+  await page.waitForSelector('div[class="col-sm-6 pl-0"] > p');
+
+  const data = await page.evaluate(() => {
+
+    const balance = document.querySelector('div[class="col-sm-6 pl-0"] > p').innerText
+    return balance
+  });
+  await browser.close();
+  return data;
+}
+
+module.exports = {
+  executeScrap
+};
